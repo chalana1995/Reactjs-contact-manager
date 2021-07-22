6ed67c0a336943b24a6aa6cb6e7b2a898bace8b2
chalana kalpitha <chalanakalpitha72@gmail.com>
2021-07-22 22:32:28 +0530

    header  and addcontact component create

--- contact-app/src/components/App.js
+++ contact-app/src/components/App.js
@@ -1,8 +1,16 @@
+import React from 'react';
 import './App.css';
+import Header from './Header';
+import AddContact from './AddContact';
+import ContactList from './ContactList';
 
 function App() {
   return (
-    <h1>Hello world</h1>
+    <div>
+      <Header />
+      <AddContact />
+      <ContactList />
+    </div>
   );
 }
 
