6a7949bf6430361d56fcb8169e153c9e6911ea3e
chalana kalpitha <chalanakalpitha72@gmail.com>
2021-07-23 00:27:27 +0530

    add form and check

--- contact-app/src/components/ContactList.js
+++ contact-app/src/components/ContactList.js
@@ -1,9 +1,20 @@
 import React from 'react'
+import ContactCard from './ContactCard';
 
-const ContactList = () => {
+const ContactList = (props) => {
+    console.log(props);
+
+    const renderContactList = props.contacts.map((contact) => {
+        return (
+           <ContactCard contact={contact} />
+        )
+    })
+
+
+
     return (
-        <div>
-            
+        <div className="ui celled list">
+            {renderContactList}
         </div>
     )
 }
