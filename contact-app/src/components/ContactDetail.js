12bfc9c7be937155e04dbda870b06878f41f3547
chalana kalpitha <chalanakalpitha72@gmail.com>
2021-07-23 21:56:19 +0530

    add server api folder and add json server

--- contact-app/src/components/ContactDetail.js
+++ contact-app/src/components/ContactDetail.js
@@ -3,7 +3,7 @@ import { Link } from 'react-router-dom';
 import user from '../images/user1.png'
 
 const ContactDetail = (props) => {
-    const {name, email} = props.location.state.contact;
+    const { name, email } = props.location.state.contact;
     return (
         <div className="main">
             <div className="ui card centered">
@@ -14,6 +14,11 @@ const ContactDetail = (props) => {
                     <div className="header">{name}</div>
                     <div className="description">{email}</div>
                 </div>
+            </div>
+            <div className="center-div" >
+                <Link to="/">
+                    <button className="ui center floated primary button" style={{ marginRight: "50%", marginLeft: "37%", width: "300px" }} >Back to the Contact List</button>
+                </Link>
             </div>
         </div>
     )
